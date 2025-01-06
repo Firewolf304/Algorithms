030d7f6a7b1d2480afdb80f4d5827e5930bdf73f
notebook <firewolf@glitchdos.>
2025-01-06 22:07:22 +0700

    test generator

---
+++ Threading/Generator_test/Generator_test/Coroutine.cpp
@@ -0,0 +1,21 @@
+﻿// Coroutine.cpp: определяет точку входа для приложения.
+//
+
+#include "Coroutine.h"
+
+Generator<std::string> test(std::string text) {
+    for (int i = 0; i < 10; i++) {
+        co_yield text + std::to_string(i);
+    }
+    co_return;
+}
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+    auto gen = test("testfunc");
+    for (int i = 0; gen; ++i) {
+        std::cout << gen() << std::endl;
+    }
+	return 0;
+}
